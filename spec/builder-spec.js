d4b9d2a99500bd185b9e610266813bd56f3d4651
Matthew Elbing <matt@Matthews-MacBook-Pro.local>
2018-07-01 18:31:12 -0400

    first

---
+++ spec/builder-spec.js
@@ -0,0 +1,73 @@
+'use babel';
+
+import Builder from '../lib/builder';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('Builder', () => {
+  let workspaceElement, activationPromise;
+
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace);
+    activationPromise = atom.packages.activatePackage('builder');
+  });
+
+  describe('when the builder:toggle event is triggered', () => {
+    it('hides and shows the modal panel', () => {
+      // Before the activation event the view is not on the DOM, and no panel
+      // has been created
+      expect(workspaceElement.querySelector('.builder')).not.toExist();
+
+      // This is an activation event, triggering it will cause the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'builder:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        expect(workspaceElement.querySelector('.builder')).toExist();
+
+        let builderElement = workspaceElement.querySelector('.builder');
+        expect(builderElement).toExist();
+
+        let builderPanel = atom.workspace.panelForItem(builderElement);
+        expect(builderPanel.isVisible()).toBe(true);
+        atom.commands.dispatch(workspaceElement, 'builder:toggle');
+        expect(builderPanel.isVisible()).toBe(false);
+      });
+    });
+
+    it('hides and shows the view', () => {
+      // This test shows you an integration test testing at the view level.
+
+      // Attaching the workspaceElement to the DOM is required to allow the
+      // `toBeVisible()` matchers to work. Anything testing visibility or focus
+      // requires that the workspaceElement is on the DOM. Tests that attach the
+      // workspaceElement to the DOM are generally slower than those off DOM.
+      jasmine.attachToDOM(workspaceElement);
+
+      expect(workspaceElement.querySelector('.builder')).not.toExist();
+
+      // This is an activation event, triggering it causes the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'builder:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        // Now we can test for view visibility
+        let builderElement = workspaceElement.querySelector('.builder');
+        expect(builderElement).toBeVisible();
+        atom.commands.dispatch(workspaceElement, 'builder:toggle');
+        expect(builderElement).not.toBeVisible();
+      });
+    });
+  });
+});
---
+++ spec/builder-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import BuilderView from '../lib/builder-view';
+
+describe('BuilderView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
